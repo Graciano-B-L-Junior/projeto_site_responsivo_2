0ecae827c2ff2910fe3a074fd01bb6ec51b7df68
Graciano Junior <gracianobarbosalimajunior@gmail.com>
2020-09-02 16:30:55 -0300

    projeto concluido

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Projeto site responsivo simples</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="site feito para testar as minhas habilidade com html e css">
+	<meta name="keywords" content="HTML,CSS3,Design responsivo">
+	<meta name="author" content="Graciano Junior">
+	<link rel="stylesheet" type="text/css" href="estilo/style.css">
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet">
+</head>
+<body>
+	<header>
+		<div class="topo">
+		<div class="center">
+		<h2 class="left">Logo</h2>
+		<h3 class="right">Titulo</h3>
+		</div>
+		<div class="clear"></div>
+		</div>
+		<div class="sub-titulo">
+			<div class="center">
+				<h4>Jogos de luta</h4>
+			</div>
+		</div>
+	</header>
+	<section class="introducao">
+		<div class="center">
+			<div class="intro-wrapper">
+			<h1>Review Streets of Rage 4: clássico dos anos 90 volta com belos gráficos</h1>
+			<p>Sair na briga de rua nunca foi tão divertido e bonito; confira a análise completa do game
+			</p>
+			</div>
+			<img src="imagens/img1.jpg" class="image-center">
+			<div class="conteudo">
+				<p><a href="">Streets of Rage 4</a> é o lançamento que traz de volta uma das séries mais queridas dos anos 90 para o PlayStation 4 (<a href="">PS4</a>),<a href=""> Xbox One</a>, <a href="">Nintendo Switch</a> e PC, via <a href="">Steam</a>. Lançada originalmente no <a href="">Mega Drive</a>, console em que fez fama, a saga Sreets of Rage foi uma das principais representantes do estilo “andar e bater”, ou “beat’em up”, como é conhecido no termo original. O novo game resgata personagens clássicos e também apresenta novidades, além de trazer gráficos 2D desenhados extremamente bem trabalhados, entre outros pontos dignos de elogio. Quer saber se Streets of Rage 4 vale a pena? Confira, no review completo a seguir, a análise de prós e contras do clássico repaginado.</p>
+
+				<h1><a href="">Streets of Rage: curiosidades e polêmicas sobre o clássico do Mega Drive</a></h1>
+			</div>
+				<img src="imagens/img2.jpg" class="image-center diminui">
+
+			<div class="conteudo">
+				<h1>De volta à cidade</h1>
+				<p>Streets of Rage 4 se passa em Wood Oak City e, novamente, lida com o tema bem básico de corrupção policial e violência, da mesma forma que vimos em títulos anteriores. Nesta versão, há o retorno de Axel Stone, Blaze Fielding e Adam Hunter como personagens clássicos, além de a estreia de Cherry Hunter e Floyd Iraia, alguns dos presentes jogáveis dentro do game. Com o tempo, mais nomes se enquadram no elenco, mas isso é apenas uma das surpresas que a aventura proporciona.
+				<br>
+				<br>
+				A história de Streets of Rage 4 se desenrola conforme avançamos nas fases, mas não há muito a se destacar. Não é nada exatamente memorável a não ser o envolvimento dos personagens com os criminosos e seu senso de justiça, que os obriga a caçar e derrubar os chefões do crime da cidade. Todo o clima de filme policial dos anos 80 está de volta, mas entenda: é este o charme.</p>
+
+				<h1>Controles nostálgicos ou modernos</h1>
+				<p>Streets of Rage 4 pode ser fruto de um retorno da série clássica, no qual seu último título foi lançado há mais de 20 anos, mas nem por isso ele usa isso como desculpa para não se modernizar. Os controles, por exemplo, foram repaginados. Há muito mais opções para o que se fazer dentro das fases, combinando botões para ativar ataques variados. Hoje, ele lembra muito mais um jogo de luta do que as versões clássicas – mas calma, não quer dizer que tenha mudado drasticamente.</p>
+
+				</div>
+				<img src="imagens/img3.jpg" class="image-center diminui" >
+			<div class="conteudo">
+				<p>A maior prova disso é que o jogo permite alternar para controles clássicos, mais básicos e direto ao ponto, para quem quiser a diversão em seu nível mais tranquilo e sem enrolações. Apesar de ter sido modernizado e repaginado, Streets of Rage 4 carrega vários traços de nostalgia dentro de si, a exemplo da trilha sonora, que também pode ser alternada entre a nova e a tradicional, lembrando as músicas da época do Mega Drive – e dando um clima todo especial para as partidas.</p>
+
+				<h1>Como funciona o jogo?</h1>
+				<p>Streets of Rage 4 segue de perto a fórmula que já vimos na série toda, caso você conheça. Se está chegando agora, não tem problema, explicamos: o game se divida em fases 2D. O personagem precisa andar sempre para a direita para avançar, lutando contra os inimigos que aparecerem pela frente em tempo real. Ao final, um chefão o aguarda, com muito mais vida e com um desafio lá no alto.</p>
+			</div>
+				<img src="imagens/img4.jpg" class="image-center diminui">
+			<div class="conteudo">
+				<p>Parece simples, e até é, mas Streets of Rage 4 adiciona mais caldo na magia. O jogo tem seus “pormenores”. É preciso aprender padrões de ataque dos inimigos, para saber como revidar, ou como atacar de maneira diferente, com o objetivo de não tomar dano. Além disso, cada um dos personagens se comportam de uma forma distinta. Axel e Blaze, por exemplo, são mais lentos que Cherry, por serem mais velhos, mas são mais fortes. Tudo é questão de costume e de eleger seu favorito para ir praticando.</p>
+				<h1>Multiplayer</h1>
+				<p>Como todo bom game no estilo “beat’em up”, Streets of Rage 4 tem modo multiplayer. Localmente, é possível jogar com até quatro participantes, cada um em um controle, como nos fliperamas antigos. Claro que isso vai exigir que o jogador tenha quatro controles, mas é sempre possível pedir que amigos tragam o seu próprio. Já o modo online é limitado para até dois participantes, mas não deixa de ser igualmente divertido, já que você pode aproveitar com qualquer pessoa do mundo.</p>
+			</div>
+				<img src="imagens/img5.jpg" class="image-center diminui">
+			<div class="conteudo">
+				<h1>Da produção</h1>
+				<p>Vale usar o review para avisar que Streets of Rage 4 não teve qualquer envolvimento direto da <a href="">SEGA</a>, sua antiga casa, apesar de fazer parte da série de maneira oficial graças à autorização concedida pela dona do <a href="">Sonic</a>. O game é fruto da união de três empresas diferentes: Dotemu, Lizardcube e Guard Crush Games. É importante citar isso por um simples motivo: bagagem prévia.</p>
+			</div>
+				<img src="imagens/img6.jpg" class="image-center diminui">
+			<div class="conteudo">
+				<p>Ser feito por outras empresas não quer dizer que o jogo desrespeite elementos clássicos ou fuja de um padrão, pelo contrário. Ele usa, por exemplo, o mesmo motor-gráfico de Streets of Fury, um game da Guard Crush Games, como base. Além disso, ele tem nomes dos times originais, como o lendário compositor Yuzo Koshiro, que rendeu alguns faixas para esta nova versão. Ou seja: espere algo que esteja dentro dos padrões já conhecidos pelos fãs.</p>
+				<h1>E os gráficos, enfim</h1>
+				<p>Streets of Rage 4 é um dos jogos 2D mais bonitos da atualidade. Simplesmente não há defeitos. Os personagens são bonitos, bem desenhados à mão, com boa proporção de tela, movimentos fluidos. Isso sem falar nos cenários. Luzes de neon refletem em todos os lugares. Efeitos climáticos mudam por inteiro o tom de uma fase.</p>
+			</div>
+				<img src="imagens/img6.jpg" class="image-center diminui">
+			<div class="conteudo">
+				<p>O trabalho visual feito em Streets of Rage 4 mostra que, sim, jogos 3D e ultra-realistas são legais e interessantes. No entanto, ainda é impossível dizer que o estilo 2D será ultrapassado ou descartado por completo, quando um trabalho tão bem feito, neste nível, é realizado. Fica a dica: quanto maior a tela em Streets of Rage 4, melhor. Os detalhes são muitos, muitos mesmo. Com várias pequenas surpresas e homenagens a jogos passados.</p>
+				<h1>Conclusão</h1>
+				<p>Streets of Rage 4 é um produto muito bom em termos de nostalgia, mas que não se prende apenas a isso. O jogo entrega um pacote completo: diversão, gráficos, som, jogabilidade e multiplayer. Há muitas opções a se explorar, destrancáveis como extras e configurações opcionais diversas. Sem falar no charme de ser um “beat’em up” bem produzido em pleno 2020, mostrando que o gênero ainda tem muito a oferecer, se bem explorado. Desde já é um dos games mais imperdíveis deste ano.</p>
+			</div>
+		</div>
+	</section>
+</body>
+</html>
